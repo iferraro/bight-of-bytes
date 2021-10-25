af7cda0847ec4a25cfdc6de85278f492e1835166
iferraro <afronerd01@outlook.com>
2021-10-25 10:09:22 -0700

    Main_app urls.py added, homepage html added

--- bight_of_bytes/settings.py
+++ bight_of_bytes/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'bight_of_bytes.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bight_of_bytes',
+        'NAME': 'bightofbytes',
     }
 }
 
