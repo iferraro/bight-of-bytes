e27ad8d141adf361b1ac5df486de27259b8838c7
iferraro <afronerd01@outlook.com>
2021-10-26 10:26:22 -0700

    Straightend out models

--- main_app/models.py
+++ main_app/models.py
@@ -2,12 +2,12 @@ from django.db import models
 #from django.contrib.auth.models import User
 
 class Device(models.Model):
-    name = models.CharField(max_length=100),
-    base_model = models.BooleanField
+    name = models.CharField(max_length=100)
+    base_model = models.BooleanField()
 
 class Variant(models.Model):
-    storage = models.IntegerField(),
-    price = models.DecimalField(),
+    storage = models.IntegerField()
+    price = models.DecimalField(decimal_places=2, max_digits=10)
     device = models.ForeignKey(Device, on_delete=models.CASCADE)
 
 print(type(Variant.device))
