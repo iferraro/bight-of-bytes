9639294cfa92bb10f5fd741e94ad8b123c09bbd1
iferraro <afronerd01@outlook.com>
2021-10-27 11:46:53 -0700

    Preloaded data working after dropping database

--- main_app/models.py
+++ main_app/models.py
@@ -4,7 +4,6 @@ from django.urls import reverse
 
 class Device(models.Model):
     name = models.CharField(max_length=100)
-    base_model = models.BooleanField()
 
     def __str__(self):
         return self.name
